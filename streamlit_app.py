c10baa0a6d971aedd9e38a13ebae9ee71354e303
keerthana-s721 <keerthanas2182@gmail.com>
2024-10-28 15:10:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 st.set_page_config(page_title="Document Genie", layout="wide")
 
 st.markdown("""
-##PDF-Exploration: Get instant insights from your Documents
+##-PDF-Exploration: Get instant insights from your Documents
 """)
 
 # API key input
